680a8b1acb7d0bed6b26ddf975071d0cb5d8b784
aytona <christopher.aytona@gmail.com>
2015-08-20 14:36:22 -0400

    Coin Placements

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+    [SerializeField]
+    private float rotateSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Spin());
+	}
+
+    private IEnumerator Spin()
+    {
+        while (true)
+        {
+            transform.Rotate(transform.up, rotateSpeed * 360 * Time.deltaTime);
+            yield return 0;
+        }
+    }
+}
